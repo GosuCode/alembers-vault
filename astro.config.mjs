10f354ec0cc0e5cd5864df5f2ffcf0f219d5522d
GosuCode <gosucode1945@gmail.com>
2026-09-19 13:28:49 +0545

    chore: set canonical site to vault.shreeshalember.com.np

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from '@astrojs/sitemap';
 // Update `site` after attaching a custom domain.
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alembers-vault.gosucode1945.workers.dev',
+  site: 'https://vault.shreeshalember.com.np',
   integrations: [react(), mdx(), sitemap()],
 
   vite: {
